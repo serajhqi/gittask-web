6f6d21e3b662d16d809fe1c0fbd55f28f519e5cd
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2025-01-18 02:02:03 +0330

    feat: add elf store and login

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,9 +1,24 @@
+import { useObservable } from "@ngneat/react-rxjs";
 import { Button, Input } from "rsuite";
+import { GetUserStore } from "../store/user.store";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate()
+  const [loading] = useObservable(GetUserStore().loginLoading$)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function onClick() {
+    GetUserStore().Login(email, password).then(() => {
+      navigate("/")
+    })
+  }
+
   return <div>
-    <Input placeholder="email" />
-    <Input type="password" placeholder="password" />
-    <Button appearance="primary">Login</Button>
+    <Input placeholder="email" onChange={(v) => setEmail(v)} value={email} />
+    <Input type="password" placeholder="password" onChange={(v) => setPassword(v)} value={password} />
+    <Button appearance="primary" loading={loading} onClick={onClick}>Login</Button>
   </div>
 }
---
+++ src/store/user.store.ts
@@ -0,0 +1,70 @@
+import { createStore, select, withProps } from "@ngneat/elf";
+import {
+  persistState,
+  localStorageStrategy,
+  excludeKeys,
+} from "@ngneat/elf-persist-state";
+import { GetApi } from "../api/api";
+
+interface UserState {
+  isLogin: boolean;
+  loginLoading: boolean;
+  token?: string;
+}
+
+const defaultState: UserState = {
+  isLogin: false,
+  loginLoading: false,
+};
+
+const store = createStore({ name: "user" }, withProps<UserState>(defaultState));
+
+export const persist = persistState(store, {
+  key: "user",
+  storage: localStorageStrategy,
+  source: () => store.pipe(excludeKeys(["isLogin", "loginLoading"])),
+});
+// ---------------------------------------------
+
+class UserStore {
+  isLogin$ = store.pipe(select((state) => state.isLogin));
+  loginLoading$ = store.pipe(select((state) => state.loginLoading));
+
+  constructor() {
+    const initialState = store.getValue();
+    if (initialState.token) {
+      store.update((state) => ({ ...state, isLogin: true }));
+    } else {
+      store.update((state) => ({ ...state, isLogin: false }));
+    }
+  }
+
+  async Login(email: string, password: string) {
+    store.update((state) => ({ ...state, loginLoading: true }));
+    try {
+      const { data } = await GetApi().POST("/user/login", {
+        body: {
+          email,
+          password,
+        },
+      });
+      store.update((state) => ({
+        ...state,
+        token: data?.token,
+        isLogin: true,
+      }));
+    } finally {
+      store.update((state) => ({ ...state, loginLoading: false }));
+    }
+  }
+}
+
+// ---------------------------------------------
+
+let userStore: UserStore;
+export function GetUserStore(): UserStore {
+  if (!userStore) {
+    userStore = new UserStore();
+  }
+  return userStore;
+}
